052705e1a0d9c178090d4bd0ba2f0dd5054ba3ff
Allohamora <herman2000@yandex.ru>
2021-09-11 20:53:33 +0300

    feat: add uk/che misteram

--- libs/pizza-parser/ukraine/chernivtsi/index.ts
+++ libs/pizza-parser/ukraine/chernivtsi/index.ts
@@ -3,8 +3,9 @@ import { Chelentano } from './chelentano';
 import { Czernowizza } from './czernowizza';
 import { Dongustavo } from './dongustavo';
 import { Lapiec } from './lapiec';
+import { Misteram } from './misteram';
 import { Monopizza } from './monopizza';
 import { PizzaIt } from './pizza-it';
 import { ShoSho } from './shosho';
 
-export default [ShoSho, Chelentano, Apetti, Monopizza, Dongustavo, Lapiec, Czernowizza, PizzaIt];
+export default [ShoSho, Chelentano, Apetti, Monopizza, Dongustavo, Lapiec, Czernowizza, PizzaIt, Misteram];
