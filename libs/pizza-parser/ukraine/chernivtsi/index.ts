30bd9ead8608b82a4d68ede6cb0e1f80590309cf
Allohamora <herman2000@yandex.ru>
2022-02-01 15:30:01 +0200

    fix(pizza-parser): fix missed monopizza bug

--- libs/pizza-parser/ukraine/chernivtsi/index.ts
+++ libs/pizza-parser/ukraine/chernivtsi/index.ts
@@ -4,8 +4,7 @@ import { Czernowizza } from './czernowizza';
 import { Dongustavo } from './dongustavo';
 import { Lapiec } from './lapiec';
 import { Misteram } from './misteram';
-import { Monopizza } from './monopizza';
 import { PizzaIt } from './pizza-it';
 import { ShoSho } from './shosho';
 
-export default [ShoSho, Chelentano, Apetti, Monopizza, Dongustavo, Lapiec, Czernowizza, PizzaIt, Misteram];
+export default [ShoSho, Chelentano, Apetti, Dongustavo, Lapiec, Czernowizza, PizzaIt, Misteram];
