8c097771773268609563f02a47a7bdd1326d8e7a
Allohamora <herman2000@yandex.ru>
2022-02-04 20:13:06 +0200

    chore(pizza-parser): refactor appeti

--- libs/pizza-parser/ukraine/chernivtsi/appeti/index.ts
+++ libs/pizza-parser/ukraine/chernivtsi/appeti/index.ts
@@ -5,26 +5,6 @@ import { ChernivtsiPizzasParser } from '../chernivtsi.pizza-parser';
 
 const BASE_URL = 'https://appeti.com.ua';
 
-const REPEATING_SPACES_REGEXP = /\s+/g;
-const INVALID_SYMBOLS_REGEXP = /, ..$/;
-
-const MEASURE_OF_SIZE = 'см';
-const MEASURE_OF_PRICE = 'грн';
-
-const CATEGORY_SELECTOR = 'section.catrgories-all div[index="pizza"]';
-const CATEGORY_ITEM_TITLE_SELECTOR = '.title';
-const CATEGORY_ITEM_TITLE_LINK_ATTR = 'href';
-
-const CARD_SELECTOR = '#msProduct';
-const CARD_TITLE_SELECTOR = '.title';
-const CARD_DESCRIPTION_SELECTOR = '.content';
-const CARD_IMAGE_SELECTOR = '#msGallery a';
-const CARD_IMAGE_LINK_ATTR = 'href';
-
-const VARIANT_FORM_SELECTOR = '.sizes form';
-const VARIANT_SIZE_SELECTOR = '.value';
-const VARIANT_PRICE_SELECTOR = '.product-price';
-
 export class Apetti extends ChernivtsiPizzasParser {
   private async getPageHtml(pageLink: string = BASE_URL) {
     return await getText(pageLink);
@@ -34,65 +14,65 @@ export class Apetti extends ChernivtsiPizzasParser {
     return `${BASE_URL}/${href}`;
   }
 
-  private parsePizzaCategoryLinks($: CheerioAPI, category: Cheerio<Element>) {
-    return category
+  private parsePizzaCategoryLinks($: CheerioAPI, $category: Cheerio<Element>) {
+    return $category
       .children()
       .toArray()
       .map((item) => {
-        const href = $(item).find(CATEGORY_ITEM_TITLE_SELECTOR).attr(CATEGORY_ITEM_TITLE_LINK_ATTR);
+        const href = $(item).find('.title').attr('href');
 
         return this.withBaseUrl(href);
       });
   }
 
   private async getPizzaLinks($: CheerioAPI) {
-    const category = $(CATEGORY_SELECTOR);
+    const category = $('section.catrgories-all div[index="pizza"]');
 
     return this.parsePizzaCategoryLinks($, category);
   }
 
-  private getCardTitle(card: Cheerio<Element>) {
-    const element = card.find(CARD_TITLE_SELECTOR);
-    const text = element.text().trim();
+  private getCardTitle($card: Cheerio<Element>) {
+    const $element = $card.find('.title');
+    const text = $element.text().trim();
 
     return lowerAndCapitalize(text);
   }
 
-  private getCardDescription(card: Cheerio<Element>) {
-    const element = card.find(CARD_DESCRIPTION_SELECTOR);
-    const text = element.text().trim();
+  private getCardDescription($card: Cheerio<Element>) {
+    const $element = $card.find('.content');
+    const text = $element.text().trim();
 
-    return text.replace(REPEATING_SPACES_REGEXP, ' ').replace(INVALID_SYMBOLS_REGEXP, '');
+    return text.replace(/\s+/g, ' ').replace(/, ..$/, '');
   }
 
-  private getCardImage(card: Cheerio<Element>) {
-    return card.find(CARD_IMAGE_SELECTOR).attr(CARD_IMAGE_LINK_ATTR);
+  private getCardImage($card: Cheerio<Element>) {
+    return $card.find('#msGallery a').attr('href');
   }
 
-  private getVariantSize(form: Cheerio<Element>) {
-    const variant = form.find(VARIANT_SIZE_SELECTOR);
-    const sizeWithMeasure = variant.text();
-    const sizeWithoutMeasure = sizeWithMeasure.replace(MEASURE_OF_SIZE, '').trim();
+  private getVariantSize($form: Cheerio<Element>) {
+    const $variant = $form.find('.value');
+    const sizeWithCM = $variant.text();
+    const sizeWithoutCM = sizeWithCM.replace('см', '').trim();
 
-    return Number(sizeWithoutMeasure);
+    return Number(sizeWithoutCM);
   }
 
-  private getVariantPrice(form: Cheerio<Element>) {
-    const variant = form.find(VARIANT_PRICE_SELECTOR);
-    const priceWithMeasure = variant.text();
-    const priceWithoutMeasure = priceWithMeasure.replace(MEASURE_OF_PRICE, '').trim();
+  private getVariantPrice($form: Cheerio<Element>) {
+    const $variant = $form.find('.product-price');
+    const priceWithGrn = $variant.text();
+    const priceWithoutGrn = priceWithGrn.replace('грн', '').trim();
 
-    return Number(priceWithoutMeasure);
+    return Number(priceWithoutGrn);
   }
 
-  private getCardVariants($: CheerioAPI, card: Cheerio<Element>) {
-    const elements = card.find(VARIANT_FORM_SELECTOR).toArray();
+  private getCardVariants($: CheerioAPI, $card: Cheerio<Element>) {
+    const elements = $card.find('.sizes form').toArray();
 
     return elements.map((element) => {
-      const form = $(element);
+      const $form = $(element);
 
-      const size = this.getVariantSize(form);
-      const price = this.getVariantPrice(form);
+      const size = this.getVariantSize($form);
+      const price = this.getVariantPrice($form);
       const weight = null; // doesn't have weight
 
       return { size, price, weight };
@@ -100,15 +80,15 @@ export class Apetti extends ChernivtsiPizzasParser {
   }
 
   private async parsePizzaPage(link: string) {
-    const page = await this.getPageHtml(link);
-    const $ = cheerio.load(page);
-    const card = $(CARD_SELECTOR);
+    const pageHtml = await this.getPageHtml(link);
+    const $ = cheerio.load(pageHtml);
+    const $card = $('#msProduct');
 
-    const title = this.getCardTitle(card);
-    const description = this.getCardDescription(card);
-    const image = this.getCardImage(card);
+    const title = this.getCardTitle($card);
+    const description = this.getCardDescription($card);
+    const image = this.getCardImage($card);
 
-    const variants = this.getCardVariants($, card);
+    const variants = this.getCardVariants($, $card);
     const base = { title, description, image, link, ...this.baseMetadata };
 
     return variants.map((variant) => ({ ...base, ...variant }));
