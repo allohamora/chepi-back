54b0b874b9af0bdaf27686b85ec0edada99093ce
Allohamora <herman2000@yandex.ru>
2022-02-04 19:16:47 +0200

    refactor(pizza-parser): refactor misteram

---
+++ libs/pizza-parser/ukraine/chernivtsi/misteram/company-list.ts
@@ -0,0 +1,110 @@
+const NUMBERS_WITH_CM = /\d+? ?см/;
+
+export interface Category {
+  slug: string;
+  id: number;
+  size: number | null;
+  blacklist?: RegExp[];
+  remove?: RegExp[];
+}
+
+export interface Company {
+  slug: string;
+  id: number;
+  categories: Category[];
+}
+
+export const COMPANY_LIST: Company[] = [
+  { slug: 'pizzapark', id: 420, categories: [{ id: 4798, size: null, slug: 'pizza' }] },
+  {
+    slug: 'panska-vtiha',
+    id: 513,
+    categories: [{ id: 6130, size: null, slug: 'pizza', blacklist: [/лаваш/, /хачапурі/] }],
+  },
+  {
+    slug: 'lapasta',
+    id: 3134,
+    categories: [{ id: 38384, size: null, slug: 'pizza', remove: [/,?\s+зауважте.+$/u], blacklist: [/фокачча/] }],
+  },
+  {
+    slug: 'baza',
+    id: 563,
+    categories: [
+      { id: 7067, size: 50, slug: 'pizza', remove: [NUMBERS_WITH_CM] },
+      { id: 7068, size: 30, slug: 'pizza30', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  {
+    slug: 'picantico',
+    id: 1103,
+    categories: [
+      { id: 13559, size: 50, slug: 'pizza50', remove: [NUMBERS_WITH_CM] },
+      { id: 13558, size: 35, slug: 'pizza35', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  {
+    slug: 'thesad',
+    id: 2096,
+    categories: [
+      { id: 26669, size: null, slug: 'pizza' },
+      { id: 32975, size: 50, slug: 'pizza50', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  { slug: 'monopoli', id: 803, categories: [{ id: 13020, size: null, slug: 'pizza' }] },
+  { slug: 'kartoteka', id: 1323, categories: [{ id: 16173, size: null, slug: 'pizza' }] },
+  { slug: 'chicheri', id: 448, categories: [{ id: 35368, size: 30, slug: 'pizza30' }] },
+  {
+    slug: 'gorno',
+    id: 1338,
+    categories: [
+      { id: 16461, size: 30, slug: 'pizza30', remove: [NUMBERS_WITH_CM] },
+      { id: 16704, size: 50, slug: 'pizza50', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  { slug: 'bruno', id: 669, categories: [{ id: 8166, size: null, slug: 'pizza' }] },
+  {
+    slug: 'mamaspizza',
+    id: 1026,
+    categories: [
+      { id: 13687, size: 30, slug: 'pizza30', remove: [NUMBERS_WITH_CM] },
+      { id: 13686, size: 50, slug: 'pizza50', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  { slug: 'riposo', id: 635, categories: [{ id: 7666, size: 30, slug: 'pizza' }] },
+  {
+    slug: 'chebonta',
+    id: 2754,
+    categories: [
+      { id: 34248, size: 50, slug: 'pizza50', remove: [NUMBERS_WITH_CM] },
+      { id: 34245, size: 35, slug: 'pizza35', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  { slug: 'terassa', id: 1124, categories: [{ id: 42425, size: 30, slug: 'pizza30' }] },
+  { slug: 'kucheri', id: 1111, categories: [{ id: 14047, size: 30, slug: 'pizza', remove: [NUMBERS_WITH_CM] }] },
+  { slug: 'kleopatra', id: 713, categories: [{ id: 14366, size: null, slug: 'pizza' }] },
+  { slug: 'ciabatta', id: 1069, categories: [{ id: 13168, size: 30, slug: 'pizza' }] },
+  {
+    slug: 'diverso',
+    id: 3076,
+    categories: [{ id: 37363, size: null, slug: 'pizzaburgers', blacklist: [/бургер/] }],
+  },
+  {
+    slug: 'yourpizza',
+    id: 3034,
+    categories: [
+      { id: 36919, size: 20, slug: 'pizza20', remove: [NUMBERS_WITH_CM] },
+      { id: 36916, size: 30, slug: 'pizza30', remove: [NUMBERS_WITH_CM] },
+      { id: 36913, size: 50, slug: 'pizza50', remove: [NUMBERS_WITH_CM] },
+    ],
+  },
+  {
+    slug: 'familybakerypizza',
+    id: 3022,
+    categories: [
+      { id: 36766, size: 30, slug: 'pizza30', remove: [/ ?Ø30/] },
+      { id: 36850, size: 40, slug: 'pizza40', remove: [/ ?Ø40/] },
+    ],
+  },
+  { slug: 'aveceasare', id: 2633, categories: [{ id: 32894, size: 30, slug: 'pizza30', remove: [NUMBERS_WITH_CM] }] },
+  { slug: 'pastamia', id: 2255, categories: [{ id: 28784, size: null, slug: 'pizza', blacklist: [/пінца/] }] },
+];
--- libs/pizza-parser/ukraine/chernivtsi/misteram/index.ts
+++ libs/pizza-parser/ukraine/chernivtsi/misteram/index.ts
@@ -2,6 +2,11 @@ import { ChernivtsiPizzasParser } from '../chernivtsi.pizza-parser';
 import { getJSON } from 'libs/pizza-parser/utils/http';
 import { join } from 'libs/pizza-parser/utils/url';
 import { capitalize } from 'libs/pizza-parser/utils/string';
+import { Company, COMPANY_LIST } from './company-list';
+
+enum MeasureType {
+  grm = '0',
+}
 
 interface Dish {
   name: string;
@@ -9,7 +14,7 @@ interface Dish {
   description: string;
   price: number;
   measure: string;
-  measureType: '0';
+  measureType: MeasureType;
   availableToOrder: boolean;
 }
 
@@ -21,166 +26,105 @@ interface Category {
   remove?: RegExp[];
 }
 
-const cmRegex = /\d+? ?см/;
+const BASE_URL = 'https://misteram.com.ua/chernivtsi';
+const API_BASE_URL = 'https://misteram.com.ua/api';
+const ASSET_BASE_URL = 'https://assets.misteram.com.ua';
+
+const MAX_ITEMS_PER_COMPANY = 100;
 
 export class Misteram extends ChernivtsiPizzasParser {
-  private maxLimit = 100;
-  private misteramLink = 'https://misteram.com.ua/chernivtsi/';
-  private misteramApiLink = 'https://misteram.com.ua/api/';
-
-  private companyList = [
-    { slug: 'pizzapark', company: 420, categories: [{ id: 4798, size: null, slug: 'pizza' }] },
-    {
-      slug: 'panska-vtiha',
-      company: 513,
-      categories: [{ id: 6130, size: null, slug: 'pizza', blacklist: [/лаваш/, /хачапурі/] }],
-    },
-    {
-      slug: 'lapasta',
-      company: 3134,
-      categories: [{ id: 38384, size: null, slug: 'pizza', remove: [/,?\s+зауважте.+$/u], blacklist: [/фокачча/] }],
-    },
-    {
-      slug: 'baza',
-      company: 563,
-      categories: [
-        { id: 7067, size: 50, slug: 'pizza', remove: [cmRegex] },
-        { id: 7068, size: 30, slug: 'pizza30', remove: [cmRegex] },
-      ],
-    },
-    {
-      slug: 'picantico',
-      company: 1103,
-      categories: [
-        { id: 13559, size: 50, slug: 'pizza50', remove: [cmRegex] },
-        { id: 13558, size: 35, slug: 'pizza35', remove: [cmRegex] },
-      ],
-    },
-    {
-      slug: 'thesad',
-      company: 2096,
-      categories: [
-        { id: 26669, size: null, slug: 'pizza' },
-        { id: 32975, size: 50, slug: 'pizza50', remove: [cmRegex] },
-      ],
-    },
-    { slug: 'monopoli', company: 803, categories: [{ id: 13020, size: null, slug: 'pizza' }] },
-    { slug: 'kartoteka', company: 1323, categories: [{ id: 16173, size: null, slug: 'pizza' }] },
-    { slug: 'chicheri', company: 448, categories: [{ id: 35368, size: 30, slug: 'pizza30' }] },
-    {
-      slug: 'gorno',
-      company: 1338,
-      categories: [
-        { id: 16461, size: 30, slug: 'pizza30', remove: [cmRegex] },
-        { id: 16704, size: 50, slug: 'pizza50', remove: [cmRegex] },
-      ],
-    },
-    { slug: 'bruno', company: 669, categories: [{ id: 8166, size: null, slug: 'pizza' }] },
-    {
-      slug: 'mamaspizza',
-      company: 1026,
-      categories: [
-        { id: 13687, size: 30, slug: 'pizza30', remove: [cmRegex] },
-        { id: 13686, size: 50, slug: 'pizza50', remove: [cmRegex] },
-      ],
-    },
-    { slug: 'riposo', company: 635, categories: [{ id: 7666, size: 30, slug: 'pizza' }] },
-    {
-      slug: 'chebonta',
-      company: 2754,
-      categories: [
-        { id: 34248, size: 50, slug: 'pizza50', remove: [cmRegex] },
-        { id: 34245, size: 35, slug: 'pizza35', remove: [cmRegex] },
-      ],
-    },
-    { slug: 'terassa', company: 1124, categories: [{ id: 42425, size: 30, slug: 'pizza30' }] },
-    { slug: 'kucheri', company: 1111, categories: [{ id: 14047, size: 30, slug: 'pizza', remove: [cmRegex] }] },
-    { slug: 'kleopatra', company: 713, categories: [{ id: 14366, size: null, slug: 'pizza' }] },
-    { slug: 'ciabatta', company: 1069, categories: [{ id: 13168, size: 30, slug: 'pizza' }] },
-    {
-      slug: 'diverso',
-      company: 3076,
-      categories: [{ id: 37363, size: null, slug: 'pizzaburgers', blacklist: [/бургер/] }],
-    },
-    {
-      slug: 'yourpizza',
-      company: 3034,
-      categories: [
-        { id: 36919, size: 20, slug: 'pizza20', remove: [cmRegex] },
-        { id: 36916, size: 30, slug: 'pizza30', remove: [cmRegex] },
-        { id: 36913, size: 50, slug: 'pizza50', remove: [cmRegex] },
-      ],
-    },
-    {
-      slug: 'familybakerypizza',
-      company: 3022,
-      categories: [
-        { id: 36766, size: 30, slug: 'pizza30', remove: [/ ?Ø30/] },
-        { id: 36850, size: 40, slug: 'pizza40', remove: [/ ?Ø40/] },
-      ],
-    },
-    { slug: 'aveceasare', company: 2633, categories: [{ id: 32894, size: 30, slug: 'pizza30', remove: [cmRegex] }] },
-    { slug: 'pastamia', company: 2255, categories: [{ id: 28784, size: null, slug: 'pizza', blacklist: [/пінца/] }] },
-  ];
-
-  private getBuyLink(...slugs: string[]) {
-    return join(this.misteramLink, ...slugs);
+  private withBaseUrl(...slugs: string[]) {
+    return join(BASE_URL, ...slugs);
   }
 
-  private getImageLink(hash: string) {
-    return `https://assets.misteram.com.ua/misteram-public/${hash}-400x0.png`;
-  }
-
-  private async getPizzasFromCategories(company: number, categories: Category[], companySlug: string) {
-    const companyLink = this.getBuyLink(companySlug);
-
-    const pizzas = await Promise.all(
-      categories.map(async ({ id, size, slug, blacklist, remove = [] }) => {
-        const url = join(
-          this.misteramApiLink,
-          '/company',
-          company.toString(),
-          `/dishes?company_category_id=${id}&limit=${this.maxLimit}&offset=0&hideImages=0&lang=ua`,
-        );
-        const parsed = await getJSON<Dish[]>(url);
-        const link = join(companyLink, slug);
-
-        const pizzaList = parsed
-          .filter(({ name }) => {
-            if (blacklist === undefined) return true;
-            return !blacklist.find((regex) => regex.test(name.trim().toLowerCase()));
-          })
-          .filter(({ availableToOrder }) => availableToOrder)
-          .map(({ name, description: rawDescription, image: hash, price, measure, measureType }) => {
-            const image = this.getImageLink(hash);
-            const weight = measureType === '0' ? parseInt(measure) : null;
-
-            const title = capitalize(remove.reduce((value, regex) => value.replace(regex, ''), name).trim());
-            const description = remove.reduce((value, regex) => value.replace(regex, ''), rawDescription).trim() || ' ';
-
-            return { title, description, image, link, price, weight, size, ...this.baseMetadata };
-          });
-
-        return pizzaList;
-      }),
+  private async getDishes(companyId: number, categoryId: number) {
+    const url = join(
+      API_BASE_URL,
+      '/company',
+      companyId.toString(),
+      `/dishes?company_category_id=${categoryId}&limit=${MAX_ITEMS_PER_COMPANY}&offset=0&hideImages=0&lang=ua`,
     );
 
-    return pizzas.flat(1);
+    return await getJSON<Dish[]>(url);
   }
 
-  private async getPizzas() {
-    const categoriesPizzas = await Promise.all(
-      this.companyList.map(async ({ slug, company, categories }) => {
-        return await this.getPizzasFromCategories(company, categories, slug);
-      }),
+  private notBlacklisted(dishName: string, blacklist?: RegExp[]) {
+    if (blacklist === undefined) return true;
+
+    const searchName = dishName.trim().toLowerCase();
+
+    return blacklist.some((regexp) => regexp.test(searchName));
+  }
+
+  private availableToOrder(availableToOrder: boolean) {
+    return availableToOrder;
+  }
+
+  private getLink(companySlug: string, categorySlug: string) {
+    return this.withBaseUrl(companySlug, categorySlug);
+  }
+
+  private getImage(hash: string) {
+    return `${ASSET_BASE_URL}/misteram-public/${hash}-400x0.png`;
+  }
+
+  private getWeight(measureType: MeasureType, measure: string) {
+    return measureType === MeasureType.grm ? parseInt(measure) : null;
+  }
+
+  private clearString(target: string, remove: RegExp[]) {
+    return remove.reduce((value, regex) => value.replace(regex, ''), target).trim();
+  }
+
+  private getTitle(dirtDishName: string, remove: RegExp[] = []) {
+    const dishName = this.clearString(dirtDishName, remove);
+
+    return capitalize(dishName);
+  }
+
+  private getDescription(dirtDishDescription: string, remove: RegExp[] = []) {
+    const dishDescription = this.clearString(dirtDishDescription, remove);
+
+    return dishDescription || ' ';
+  }
+
+  private dishToPizza(company: Company, category: Category, dish: Dish) {
+    const link = this.getLink(company.slug, category.slug);
+    const image = this.getImage(dish.image);
+    const weight = this.getWeight(dish.measureType, dish.measure);
+
+    const title = this.getTitle(dish.name, category.remove);
+    const description = this.getDescription(dish.description, category.remove);
+
+    return { title, description, image, link, weight, size: category.size, price: dish.price, ...this.baseMetadata };
+  }
+
+  private async getPizzasFromCategory(company: Company, category: Category) {
+    const dishes = await this.getDishes(company.id, category.id);
+
+    return dishes
+      .filter((dish) => this.notBlacklisted(dish.name, category.blacklist))
+      .filter((dish) => this.availableToOrder(dish.availableToOrder))
+      .map((dish) => this.dishToPizza(company, category, dish));
+  }
+
+  private async getPizzasFromCompany(company: Company) {
+    const companyNestedPizzas = await Promise.all(
+      company.categories.flatMap((category) => this.getPizzasFromCategory(company, category)),
     );
 
-    return categoriesPizzas.flat(1);
+    return companyNestedPizzas.flat();
+  }
+
+  private async getPizzasFromCompanyList() {
+    const companyListNestedPizzas = await Promise.all(
+      COMPANY_LIST.map((company) => this.getPizzasFromCompany(company)),
+    );
+
+    return companyListNestedPizzas.flat();
   }
 
   public async parsePizzas() {
-    const pizzas = await this.getPizzas();
-
-    return pizzas;
+    return await this.getPizzasFromCompanyList();
   }
 }
