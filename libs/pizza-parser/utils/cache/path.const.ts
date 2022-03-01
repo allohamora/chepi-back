37979fac03eb56065ac4b73626eb4b144fd2d37a
allohamora <herman30698@gmail.com>
2022-03-01 16:03:52 +0200

    feat(pizza-parser): improve and refactor cache
    apply gof:strategy to cache, add fs-strategy(old), in-memory-strategy and memory-fs-strategy.
    apply memory-fs-strategy to all cache instances.

---
+++ libs/pizza-parser/utils/cache/path.const.ts
@@ -0,0 +1,3 @@
+import { resolve } from 'node:path';
+
+export const CACHE_DIR = resolve(__dirname, '../..', '.cache');
--- libs/pizza-parser/utils/http.ts
+++ libs/pizza-parser/utils/http.ts
@@ -1,7 +1,8 @@
 import { HttpRequest } from 'libs/http';
+import { MemoryFsStrategy } from './cache/memory-fs.strategy';
 import { Cache } from './cache';
 
-const cache = new Cache('http');
+const cache = new Cache(new MemoryFsStrategy('http'));
 
 export const getText = cache.decorator(async (url: string) => {
   const { data } = await new HttpRequest(url).returnType('text').request<string>();
