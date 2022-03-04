6e2842b56113b481b462685d53dd0efbcc3e5023
allohamora <herman30698@gmail.com>
2022-03-04 14:12:54 +0200

    chore: fix beautiful-sort/import invalid regexp in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     'beautiful-sort/import': [
       'error',
       {
-        special: ['^node:'],
+        special: ['/^node:/'],
         order: ['special', 'namespace', 'default', 'defaultObj', 'obj', 'none'],
       },
     ],
