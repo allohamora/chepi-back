a86e7a488c332f0bb574780501ecf295ffee9a2b
allohamora <herman30698@gmail.com>
2022-03-14 14:31:16 +0200

    refactor(pizza-parser): remove console.log in shosho

--- libs/pizza-parser/ukraine/chernivtsi/shosho/index.ts
+++ libs/pizza-parser/ukraine/chernivtsi/shosho/index.ts
@@ -121,8 +121,6 @@ export class ShoSho extends ChernivtsiPizzasParser {
       const description = this.normalizeDescription(pizzaDescription);
       const image = this.getImage($modal);
 
-      console.log({ title, description });
-
       const variants = this.getVariants($, $modal);
 
       const base = { title, description, image, link: BASE_URL, ...this.baseMetadata };
